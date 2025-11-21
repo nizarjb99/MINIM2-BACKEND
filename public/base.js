8c4447d5b54ec09af580d519cdc2e1b8ec161ba8
manelcolominas <manel.colominas@estudiantat.upc.edu>
2025-11-21 09:19:15 +0100

    Automatizació URL

--- public/base.js
+++ public/base.js
@@ -1,1 +1,7 @@
-const BASE_URL = "http://localhost:8080/example/eetacbros";
+let BASE_URL;
+
+if (window.location.host.includes("localhost")) {
+    BASE_URL = "http://localhost:8080/example/eetacbros";
+} else {
+    BASE_URL = "https://dsa3.upc.edu/example/eetacbros";
+}
